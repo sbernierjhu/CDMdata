96c34ffdbecc110754d17490291375b98c25d6de
sbernierjhu <sbernier@jhu.edu>
2025-07-03 01:44:00 -0400

    begin case study code sharing

---
+++ Data used in paper/Case study 1/Case study 1 code.R
@@ -0,0 +1,10 @@
+#load in a bunch of useful packages
+library(tidyverse)
+library(RColorBrewer)
+library(SMBanalysis)
+library(extrafont)
+library(ggrepel)
+library(cowplot)
+#font_import()
+#loadfonts(device="win")       #Register fonts for Windows bitmap output
+#fonts()
